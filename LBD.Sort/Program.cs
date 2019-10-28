8f1bb5b2c3a0d8ad04f6d1f457542e394e6513f6
烂布袋 <873448329@qq.com>
2019-10-28 21:58:21 +0800

    sort

--- LBD.Sort/Program.cs
+++ LBD.Sort/Program.cs
@@ -6,17 +6,22 @@ namespace LBD.Sort
     {
         static void Main(string[] args)
         {
-            var result=Searn(new int[3] { 5,3,20},3);
+            var result=Searn(new int[] { 5,3,20,7,8,22},4);
             Console.WriteLine(result) ;
             Console.ReadKey();
         }
 
+
         public static int Searn(int[] arr,int value)
         {
+            if (arr[0]==value)
+            {
+                return 1;
+            }
             int i;
-            arr[0] = value;
+            //arr[0] = value;
             i = arr.Length-1;
-            while (arr[i]!=value)
+            while (arr[i]!=value&&i!=0)
             {
                 i--;
             }
