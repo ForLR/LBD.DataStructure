55ce5c47f56775f80cbab12b98e16c1476e3d6a6
烂布袋 <873448329@qq.com>
2020-02-29 11:46:28 +0800

    添加插入排序和选择排序方法实现

--- LBD.Sorting/Program.cs
+++ LBD.Sorting/Program.cs
@@ -12,16 +12,20 @@ namespace LBD.Sorting
         static void Main(string[] args)
         {
 
-            int[] data = new int[] { 3,2,4,1,5};
+            int[] data = new int[] { 3, 2, 4, 1, 5 };
 
             int[] data1 = new int[] { 3, 2, 4, 1, 5 };
-            CodeTimer.Initialize();
+             CodeTimer.Initialize();
 
-            BubbleSort(data);
+            SelectSort(data);
+            
+            //InsertSort(data);
 
-            ShellSort(data1);
+            //BubbleSort(data);
 
-         
+            //ShellSort(data1);
+
+
             Console.ReadKey();
         }
 
@@ -30,7 +34,7 @@ namespace LBD.Sorting
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="arr"></param>
-        public static void BubbleSort<T>(T[] arr) where T: IComparable
+        public static void BubbleSort<T>(T[] arr) where T : IComparable
         {
             CodeTimer.Time("BubbleSort", 1, () => {
                 int len = arr.Length;
@@ -38,15 +42,15 @@ namespace LBD.Sorting
 
                 for (int i = 0; i < len; i++)
                 {
-                    for (int j = i+1; j < len; j++)
+                    for (int j = i + 1; j < len; j++)
                     {
-                        if (arr[i].CompareTo(arr[j])>0)
+                        if (arr[i].CompareTo(arr[j]) > 0)
                         {
                             var temp = arr[i];
                             arr[i] = arr[j];
                             arr[j] = temp;
                         }
-                 
+
                     }
                 }
             });
@@ -75,6 +79,66 @@ namespace LBD.Sorting
                     }
                 }
             });
+        }
+
+
+        /// <summary>
+        /// 找到最小值往左边以此比较 到对应位置插入
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="arr"></param>
+        public static void InsertSort<T>(T[] arr) where T : IComparable
+        {
+            if (arr == null || arr.Length < 1)
+            {
+                return;
+            }
+            int inner;
+            T temp;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                temp = arr[i];
+                inner = i;
+                while (inner > 0 && arr[inner - 1].CompareTo(temp) > 0)
+                {
+                    arr[inner] = arr[inner - 1];
+                    inner -= 1;
+                }
+                arr[inner] = temp;
+
+            }
+
+        }
+       
+
+        /// <summary>
+        /// 循环查找最小值 互换找到的位置
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="arr"></param>
+        public static void  SelectSort<T>(T[] arr) where T : IComparable
+        {
+            if (arr == null || arr.Length < 1)
+            {
+                return;
+            }
+            T temp;
+            int min;
+            for (int outer = 0; outer < arr.Length; outer++)
+            {
+                min = outer;
+                for (int inner = outer+1; inner < arr.Length; inner++)
+                {
+                    if (arr[min].CompareTo(arr[inner])>0)
+                    {
+                        min = inner;  
+                    }
+                }
+                temp = arr[outer];
+                arr[outer] = arr[min];
+                arr[min] = temp;
+            }
+
         }
     }
 
